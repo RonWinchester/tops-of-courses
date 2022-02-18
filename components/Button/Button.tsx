c36610db05b6c9518d3a113531421104edacf2ec
Ron Winchester <rom0203@gmail.com>
2022-02-18 22:11:18 +0300

    feat: создание компонентов

---
+++ components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonProps } from "./Button.props";
+import styles from "./Button.module.css";
+import cn from "classnames";
+import ArrowIcon from "./arrow.svg";
+
+export const Button = ({
+  appearance,
+  children,
+  arrow = "none",
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={cn(styles.button, className, styles[appearance])}
+      {...props}
+    >
+      {children}
+      {arrow !== "none" && (
+        <span className={cn(styles.arrow, styles[arrow])}>
+          <ArrowIcon />
+        </span>
+      )}
+    </button>
+  );
+};

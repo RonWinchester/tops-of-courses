2d1789abf803c45e1fa9a6ed50bc7480843c4326
Ron Winchester <rom0203@gmail.com>
2022-02-26 23:40:22 +0300

    feat: footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,14 @@
 import { IFooter } from "./Footer.props";
 import style from './Footer.module.css'
 import cn from 'classnames'
+import {format} from 'date-fns'
 
-
-export const Footer = ({...props}: IFooter): JSX.Element => {
+export const Footer = ({className ,...props}: IFooter): JSX.Element => {
   return (
-    <header {...props}>
-      Footer
-    </header>
+    <footer className={cn(className, style.footer)} {...props}>
+      <p>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</p>
+      <a href="#" target='_blank'>Пользовательское соглашение</a>
+      <a href="#" target='_blank'>Политика конфиденциальности</a>
+    </footer>
   )
 }

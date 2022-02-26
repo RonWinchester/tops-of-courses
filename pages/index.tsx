ad464778150ae69858c991e8456e781338316bdc
Ron Winchester <rom0203@gmail.com>
2022-02-26 20:38:54 +0300

    feat: HOC include

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Button, Htag, Paragraph, Tag } from "../components/";
+import React from "react";
+import { Button, Htag, Paragraph, Rating, Tag } from "../components/";
+import { withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
+  const [state, useState] = React.useState<number>(4);
+
   return (
-    <div>
+    <>
       <Htag tag="h1">Текст</Htag>
       <Button appearance={"primary"} arrow="right">
         Кнопка
       </Button>
-      <Paragraph size="s">
-        Маленький
-      </Paragraph>
-      <Paragraph>
-        Средний
-      </Paragraph>
-      <Paragraph size="l">
-        Большой
-      </Paragraph>
-      <Tag color="red" size="m">Red</Tag>
-      <Tag color="primary" size="l">primary</Tag>
-      <Tag color="grey" href="yandex.ru">grey</Tag>
-    </div>
+      <Paragraph size="s">Маленький</Paragraph>
+      <Paragraph>Средний</Paragraph>
+      <Paragraph size="l">Большой</Paragraph>
+      <Tag color="red" size="m">
+        Red
+      </Tag>
+      <Tag color="primary" size="l">
+        primary
+      </Tag>
+      <Tag color="grey" href="yandex.ru">
+        grey
+      </Tag>
+      <Rating rating={state} isEditable={true} setRating={useState}></Rating>
+    </>
   );
 }
+
+export default withLayout(Home);

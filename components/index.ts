5c27b6ebab412a76bea222bade660db463dd0919
Ron Winchester <rom0203@gmail.com>
2022-04-09 12:10:15 +0300

    fix: перенос header, footer, sidebar в layout

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,3 @@ export * from './Button/Button';
 export * from './Paragraph/Paragraph';
 export * from './Tag/Tag';
 export * from './Rating/Rating';
-export * from './Sidebar/Sidebar';
-export * from './Header/Header';
-export * from './Footer/Footer';

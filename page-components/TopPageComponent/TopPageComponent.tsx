78c90fff87aa1818c0e3da0d8ecfd4ab89e5eb0a
Ron Winchester <rom0203@gmail.com>
2022-06-27 23:21:52 +0300

    feat: feat компонент страницы - вакансии

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,6 +1,8 @@
 import styles from "./TopPageComponent.module.css";
 import cn from "classnames";
 import { TopPageComponentProps } from "./TopPageComponent.props";
+import { Card, HhData, Htag, Tag } from "../../components";
+import { TopLevelCategory } from "../../interfaces/page.interface";
 
 export const TopPageComponent = ({
   page,
@@ -8,5 +10,29 @@ export const TopPageComponent = ({
   firstCategory,
   ...props
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={cn(styles.wrapper)}>
+      <div className={cn(styles.title)}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <span>Sort</span>
+      </div>
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={cn(styles.hhTitle)}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
+      </div>
+      {firstCategory == TopLevelCategory.Courses && (
+        <HhData {...page.hh}></HhData>
+      )}
+    </div>
+  );
 };
